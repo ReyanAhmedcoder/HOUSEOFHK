9340c2b3e929b1e3e2bff58851809eff9c983fd9
ReyanAhmedcoder <reyan@example.com>
2026-08-01 17:12:01 +0530

    Wire Firebase config into shared sync

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,13 @@
-window.HOHK_FIREBASE_CONFIG = null;
+window.HOHK_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyB7i73Q-pVAeYOOizSLJd1YJk7XcaC5GJ4",
+  authDomain: "houseofhk.firebaseapp.com",
+  databaseURL: "https://houseofhk-default-rtdb.firebaseio.com",
+  projectId: "houseofhk",
+  storageBucket: "houseofhk.firebasestorage.app",
+  messagingSenderId: "161424381394",
+  appId: "1:161424381394:web:801411e7a537ed4ab3240b",
+  measurementId: "G-WY466RP0WX"
+};
 
-// Example configuration:
-// window.HOHK_FIREBASE_CONFIG = {
-//   apiKey: "YOUR_API_KEY",
-//   authDomain: "YOUR_PROJECT.firebaseapp.com",
-//   databaseURL: "https://YOUR_PROJECT.firebaseio.com",
-//   projectId: "YOUR_PROJECT",
-//   storageBucket: "YOUR_PROJECT.appspot.com",
-//   messagingSenderId: "YOUR_SENDER_ID",
-//   appId: "YOUR_APP_ID"
-// };
+window.firebaseConfig = window.HOHK_FIREBASE_CONFIG;
+// 
